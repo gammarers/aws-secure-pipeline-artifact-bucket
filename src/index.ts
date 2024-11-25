1d789528e44ef2e437f7bcc77c9d32cd27273a32
yicr <yicr@users.noreply.github.com>
2024-11-25 17:40:51 +0900

    feat!: first happy code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { SecureBucket, SecureBucketProps } from '@gammarers/aws-secure-bucket';
+import { Stack } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+
+export interface SecurePipelineArtifaceBucketProps extends SecureBucketProps {}
+
+export class SecurePipelineArtifaceBucket extends SecureBucket {
+
+  constructor(scope: Construct, id: string, props?: SecurePipelineArtifaceBucketProps) {
+    super(scope, id, props);
+
+    // 👇 Get account & region
+    const account = Stack.of(this).account;
+    const region = Stack.of(this).region;
+    // 👇 Get qualifier
+    const qualifier = Stack.of(this).synthesizer.bootstrapQualifier || 'hnb659fds';
+
+    this.addToResourcePolicy(new iam.PolicyStatement({
+      actions: [
+        's3:*',
+      ],
+      resources: [
+        `${this.bucketArn}`,
+        `${this.bucketArn}/*`,
+      ],
+      principals: [
+        new iam.ArnPrincipal(`arn:aws:iam::${account}:role/cdk-${qualifier}-deploy-role-${account}-${region}`),
+      ],
+    }));
   }
+
 }
---
+++ test/buket.default.test.ts
@@ -0,0 +1,84 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { SecurePipelineArtifaceBucket } from '../src';
+
+describe('SecurePipelineArtifaceBucket default Testing', () => {
+
+  const stack = new Stack(new App(), 'TestingStack', {
+  });
+
+  const bucket = new SecurePipelineArtifaceBucket(stack, 'SecurePipelineArtifaceBucket');
+
+  it('Is Bucket', () => {
+    expect(bucket).toBeInstanceOf(s3.Bucket);
+  });
+
+  const template = Template.fromStack(stack);
+
+  it('Should have encryption', () => {
+    template.hasResourceProperties('AWS::S3::BucketPolicy', {
+      Bucket: Match.objectEquals({
+        Ref: Match.stringLikeRegexp('SecurePipelineArtifaceBucket'),
+      }),
+      PolicyDocument: {
+        Version: '2012-10-17',
+        Statement: Match.arrayWith([
+          Match.objectEquals({
+            Action: 's3:*',
+            Effect: 'Allow',
+            Principal: {
+              AWS: {
+                'Fn::Join': [
+                  '',
+                  [
+                    'arn:aws:iam::',
+                    {
+                      Ref: 'AWS::AccountId',
+                    },
+                    ':role/cdk-hnb659fds-deploy-role-',
+                    {
+                      Ref: 'AWS::AccountId',
+                    },
+                    '-',
+                    {
+                      Ref: 'AWS::Region',
+                    },
+                  ],
+                ],
+              },
+            },
+            Resource: [
+              {
+                'Fn::GetAtt': [
+                  Match.stringLikeRegexp('SecurePipelineArtifaceBucket'),
+                  'Arn',
+                ],
+              },
+              {
+                'Fn::Join': [
+                  '',
+                  [
+                    {
+                      'Fn::GetAtt': [
+                        Match.stringLikeRegexp('SecurePipelineArtifaceBucket'),
+                        'Arn',
+                      ],
+                    },
+                    '/*',
+                  ],
+                ],
+              },
+            ],
+          }),
+        ]),
+      },
+    });
+  });
+
+
+  it('Should match snapshot', () => {
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+
+});
