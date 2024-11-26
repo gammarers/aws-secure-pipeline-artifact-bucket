0737e64d143cd7ad7740d756a5b8ee4986f18703
yicr <yicr@users.noreply.github.com>
2024-11-26 15:59:05 +0900

    fix: deps

--- .projenrc.ts
+++ .projenrc.ts
@@ -8,11 +8,13 @@ const project = new awscdk.AwsCdkConstructLibrary({
   jsiiVersion: '5.5.x',
   defaultReleaseBranch: 'main',
   name: '@gammarers/aws-secure-pipeline-artifact-bucket',
+  description: 'This S3 bucket is the artifact bucket used for CDK pipelines, created when the --qualifier option is specified during the CDK bootstrap process. If you specify the --qualifier option or explicitly define an artifact bucket for CDK pipelines, you need to configure a policy that allows access to this bucket from the CDK deploy role.',
+  keywords: ['aws', 'cdk', 'aws-cdk', 'bucket', 'pipeline', 'artifact', 'qualifier'],
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers/aws-secure-pipeline-artifact-bucket.git',
-  deps: [
-    '@gammarers/aws-secure-bucket@^2.0.23',
-  ],
+  //  deps: [
+  //    '@gammarers/aws-secure-bucket@^2.0.23',
+  //  ],
   peerDeps: [
     '@gammarers/aws-secure-bucket@^2.0.23',
   ],
